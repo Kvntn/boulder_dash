2700baa434794e02aa71d3140d1f423478be3aee
N2oo <gauthier.sannier@viacesi.fr>
2019-05-29 09:47:39 +0200

    commit 11.8
    Modification des classes TheCharacters et MobileEntity

--- entity/src/main/java/entity/mobile/TheCharacter.java
+++ entity/src/main/java/entity/mobile/TheCharacter.java
@@ -12,35 +12,15 @@ public class TheCharacter extends MobileEntity {
 	public TheCharacter() {
 		
 	}
-
-	@Override
-	public boolean isAlive() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
 	@Override
 	public boolean canMove() {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
-	@Override
-	public Point getPosition() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setPosition(Point position) {
-		// TODO Auto-generated method stub
-		
-	}
-
 	@Override
 	public void setHasMoved() {
 		// TODO Auto-generated method stub
 		
 	}
-	
+
 }
